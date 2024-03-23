3f9884bc4272ca1b3c5d74a4a35eee706f7b5c1f
Syed Muhammad Kamran <smkamran2012@gmail.com>
2024-03-24 02:52:06 +0500

    number guess game

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const RandomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userguessedNumber",
+        type: "number",
+        message: " please guess a number between 1-6 : ",
+    },
+]);
+if (answer.userguessedNumber === RandomNumber) {
+    console.log("Congratulation you guessed right number.");
+}
+else {
+    console.log("You gussed wrong number.");
+}
